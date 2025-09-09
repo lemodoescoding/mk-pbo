5d7a783f79a1a2bc61c593833b58a0a036af932d
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2025-09-09 14:52:52 +0700

    feat: add pertemuan3, contains single java file

---
+++ pertemuan3/TicketMachine.java
@@ -0,0 +1,55 @@
+/**
+ * TicketMachine - PBO B - Pertemuan 3
+ *
+ * @author Ahmad Satrio Arrohman
+ * @version 2025.09.09
+ */
+public class TicketMachine {
+  private int price;
+  private int balance;
+  private int total;
+
+  public TicketMachine(int cost) {
+    price = cost;
+    balance = 0;
+    total = 0;
+  }
+
+  public int getPrice() { return price; }
+
+  public int getBalance() { return balance; }
+
+  public void insertMoney(int amount) {
+    if (amount > 0) {
+      balance = balance + amount;
+    } else {
+      System.out.println("Use a positive amount rather than: " + amount);
+    }
+  }
+
+  public void printTicket() {
+    if (balance >= price) {
+      System.out.println("##################");
+      System.out.println("# The BlueJ Line");
+      System.out.println("# Ticket");
+      System.out.println("# " + price + " cents.");
+      System.out.println("##################");
+      System.out.println();
+
+      total = total + price;
+
+      balance = balance - price;
+    } else {
+      System.out.println("You must insert at least: " + (price - balance) +
+                         " cents.");
+    }
+  }
+
+  public int refundBalance() {
+    int amountToRefund;
+    amountToRefund = balance;
+    balance = 0;
+
+    return amountToRefund;
+  }
+}
